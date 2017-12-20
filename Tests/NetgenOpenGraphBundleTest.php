c95c62024354db922bf7ec42b37fe9bd6750e083
Edi Modrić <edi.modric@gmail.com>
2017-12-20 14:36:47 +0100

    Update tests

--- Tests/NetgenOpenGraphBundleTest.php
+++ Tests/NetgenOpenGraphBundleTest.php
@@ -3,6 +3,7 @@
 namespace Netgen\Bundle\OpenGraphBundle\Tests;
 
 use Netgen\Bundle\OpenGraphBundle\DependencyInjection\Compiler\MetaTagHandlersCompilerPass;
+use Netgen\Bundle\OpenGraphBundle\DependencyInjection\Compiler\TwigRuntimePass;
 use Netgen\Bundle\OpenGraphBundle\NetgenOpenGraphBundle;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -16,9 +17,13 @@ class NetgenOpenGraphBundleTest extends TestCase
             ->setMethods(array('addCompilerPass'))
             ->getMock();
 
-        $container->expects($this->once())
+        $container->expects($this->at(0))
             ->method('addCompilerPass')
             ->with(new MetaTagHandlersCompilerPass());
+
+        $container->expects($this->at(1))
+            ->method('addCompilerPass')
+            ->with(new TwigRuntimePass());
 
         $bundle = new NetgenOpenGraphBundle();
         $bundle->build($container);
