12c41895d41d114cc77b6a4d9b780ad7a99d7c06
Edi Modrić <edi.modric@gmail.com>
2022-02-21 10:36:34 +0100

    Fix exception namespace

--- tests/Handler/Literal/TextTest.php
+++ tests/Handler/Literal/TextTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\OpenGraphBundle\Tests\Handler\Literal;
 
-use Ibexa\Contracts\Core\Exception\InvalidArgumentException;
+use Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException;
 use Netgen\Bundle\OpenGraphBundle\Handler\HandlerInterface;
 use Netgen\Bundle\OpenGraphBundle\Handler\Literal\Text;
 use Netgen\Bundle\OpenGraphBundle\MetaTag\Item;
