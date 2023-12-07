1f9dc9f52c12d26b4a216597a203847443f4c384
Miljenko Muha <miljenko@netgen.io>
2023-12-07 14:20:57 +0100

    NGSTACK-807 added handler for canonical url

---
+++ bundle/Handler/FieldType/CanonicalUrl.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\OpenGraphBundle\Handler\FieldType;
+
+use Ibexa\Contracts\Core\Repository\Values\Content\Field;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Netgen\Bundle\OpenGraphBundle\MetaTag\Item;
+use Symfony\Component\Routing\RouterInterface;
+class CanonicalUrl extends Handler
+
+{
+    private RouterInterface $router;
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+    public function getMetaTags($tagName, array $params = []): array
+    {
+        $value = $this->router->generate(
+            'ibexa.url.alias',
+            [
+                'locationId' => $this->content->contentInfo->mainLocationId,
+            ],
+            UrlGeneratorInterface::ABSOLUTE_URL,
+        );
+        return array(
+            new Item(
+                $tagName,
+                $value
+            ),
+        );
+    }
+    protected function supports(Field $field): bool
+    {
+        return true;
+    }
+}
