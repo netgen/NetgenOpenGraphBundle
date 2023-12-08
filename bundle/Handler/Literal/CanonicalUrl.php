42accf0bb9d0c4e7f0dadc98276d4fcdb180419d
Edi Modrić <edi.modric@gmail.com>
2023-12-08 18:25:03 +0100

    Fix CS

--- bundle/Handler/Literal/CanonicalUrl.php
+++ bundle/Handler/Literal/CanonicalUrl.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\OpenGraphBundle\Handler\Literal;
 
+use Ibexa\Contracts\Core\Repository\Values\Content\Content as IbexaContent;
 use Ibexa\Core\MVC\Symfony\Routing\UrlAliasRouter;
 use Netgen\Bundle\OpenGraphBundle\Handler\HandlerInterface;
 use Netgen\Bundle\OpenGraphBundle\MetaTag\Item;
+use Netgen\IbexaSiteApi\API\Values\Content as SiteApiContent;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Netgen\IbexaSiteApi\API\Values\Content as SiteApiContent;
-use Ibexa\Contracts\Core\Repository\Values\Content\Content as IbexaContent;
 
 final class CanonicalUrl implements HandlerInterface
 {
